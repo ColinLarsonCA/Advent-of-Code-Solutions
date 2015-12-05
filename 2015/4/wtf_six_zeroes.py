fca6eb666de9aa05fd36206f5f2d6abdaae20b7c
Colin <dev.colinl@gmail.com>
2015-12-04 19:48:36 -0600

    Solutions for day 4.

---
+++ 2015/4/wtf_six_zeroes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import hashlib
+
+orig_hash = hashlib.md5()
+orig_hash.update('iwrupvqb')
+
+i = 1
+while True:
+  new_hash = orig_hash.copy()
+  new_hash.update(str(i))
+  hash_str = new_hash.hexdigest()
+  if hash_str.startswith('000000'):
+    print i
+    break
+  i += 1
